f13559308d536a236602ca93fa37c081218efc47
Breno <bhfs.2908@gmail.com>
2022-04-07 11:53:49 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import { Sidebar } from '../components/Sidebar'
+
+const Home: NextPage = () => {
+  return (
+    <div className="h-screen overflow-hidden bg-black">
+      <main>
+        <Sidebar />
+        {/* Center */}
+      </main>
+
+      <div>{/* Player */}</div>
+    </div>
+  )
+}
+
+export default Home
