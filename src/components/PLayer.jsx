28b4cc199b2c249cb7f54fb47d62172d31b6794a
Breno <bhfs.2908@gmail.com>
2022-04-10 19:57:11 -0300

    feat: finish

--- src/components/PLayer.jsx
+++ src/components/PLayer.jsx
@@ -12,11 +12,12 @@ import {
   SwitchHorizontalIcon,
 } from '@heroicons/react/solid'
 import { useSession } from 'next-auth/react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { useRecoilState } from 'recoil'
 import { currentTrackIdState, isPlayingState } from '../atoms/songAtom'
 import { useSongInfo } from '../hooks/useSongInfo'
 import { useSpotify } from '../hooks/useSpotify'
+import { debounce } from 'lodash'
 
 export function Player() {
   const spotifyApi = useSpotify()
@@ -24,7 +25,7 @@ export function Player() {
   const [currentTrackId, setCurrentIdTrack] =
     useRecoilState(currentTrackIdState)
   const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState)
-  const [volume, setVolume] = useState(50)
+  const [volume, setVolume] = useState(90)
 
   const songInfo = useSongInfo()
 
@@ -55,25 +56,49 @@ export function Player() {
   useEffect(() => {
     if (spotifyApi.getAccessToken() && !currentTrackId) {
       fetchCurrentSong()
-      setVolume(50)
+      setVolume(90)
     }
   }, [currentTrackIdState, spotifyApi, session])
 
+  const debouncedAdjustVolume = useCallback(
+    debounce((volume) => {
+      spotifyApi.setVolume(volume).catch((err) => {})
+    }, 300),
+    []
+  )
+
+  useEffect(() => {
+    if (volume > 0 && volume < 100) {
+      debouncedAdjustVolume(volume)
+    }
+  }, [volume])
+
   return (
-    <div className="grid h-24 grid-cols-3 bg-gradient-to-b from-black to-gray-900 px-2 text-xs text-white md:px-8 md:text-base">
+    <div className="grid h-24 grid-cols-3 border-t-2 border-gray-900 bg-[#111] px-2 text-xs text-white md:px-8 md:text-base">
       <div className="flex items-center space-x-4">
         <img
-          className="hidden h-10 w-10 md:inline"
+          className="hidden h-14 w-14 md:inline"
           src={songInfo?.album.images?.[0]?.url}
           alt=""
         />
-        <div>
-          <h3>{songInfo?.name}</h3>
-          <p>{songInfo?.artists?.[0]?.name}</p>
+        <div className="flex items-center space-x-4">
+          <div>
+            <h3 className="text-sm">{songInfo?.name}</h3>
+            <p className="text-xs text-gray-400">
+              {songInfo?.artists?.map((artist, index) => {
+                if (index + 1 === songInfo?.artists?.length) {
+                  return `${artist.name}`
+                } else {
+                  return `${artist.name}, `
+                }
+              })}
+            </p>
+          </div>
+          <HeartIcon className="h-5 w-5 text-gray-400" />
         </div>
       </div>
 
-      <div className="flex items-center justify-evenly">
+      <div className="flex items-center justify-evenly px-32">
         <SwitchHorizontalIcon className="button" />
         <RewindIcon className="button" />
         {isPlaying ? (
