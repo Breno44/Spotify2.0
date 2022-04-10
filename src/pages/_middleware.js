5b70e159f100dc4fd44cf08c3af63ef65cba64cc
Breno Henrique <bhfs.2908@gmail.com>
2022-04-10 20:29:41 -0300

    Update _middleware.js

--- src/pages/_middleware.js
+++ src/pages/_middleware.js
@@ -11,6 +11,6 @@ export async function middleware(req) {
   }
 
   if (!token && pathname !== '/login') {
-    return NextResponse.redirect('https://spotify2-0-gray.vercel.app/login')
+    return NextResponse.redirect('/login')
   }
 }
